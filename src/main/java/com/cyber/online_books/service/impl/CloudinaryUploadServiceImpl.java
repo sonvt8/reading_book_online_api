8b5336beae4724568c3b940a803edb11fbcea736
tin <tinsatthu1988@gmail.com>
2021-12-26 08:40:31 +0700

    create CloudinaryUploadService

---
+++ src/main/java/com/cyber/online_books/service/impl/CloudinaryUploadServiceImpl.java
@@ -0,0 +1,47 @@
+package com.cyber.online_books.service.impl;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Singleton;
+import com.cloudinary.utils.ObjectUtils;
+import com.cyber.online_books.service.CloudinaryUploadService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.Map;
+
+public class CloudinaryUploadServiceImpl implements CloudinaryUploadService {
+    private Cloudinary cloudinary;
+
+    @Value("${cloudinary.cloudname}")
+    private String cloudname;
+
+    @Value("${cloudinary.apisecret}")
+    private String apisecret;
+
+    @Value("${cloudinary.apikey}")
+    private String apikey;
+
+    @PostConstruct
+    public void initializeCloudinary() {
+        cloudinary = Singleton.getCloudinary();
+        cloudinary.config.cloudName = cloudname;
+        cloudinary.config.apiKey = apikey;
+        cloudinary.config.apiSecret = apisecret;
+    }
+
+    @Override
+    public String upload(MultipartFile sourceFile, String fileName) {
+        try {
+
+            Map params = ObjectUtils.asMap("public_id", "truyenonline/" + fileName);
+            cloudinary.uploader().upload(sourceFile.getBytes(), params);
+
+            //Lấy đường dẫn ảnh vừa upload
+            return cloudinary.url().generate("truyenonline/" + fileName);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
