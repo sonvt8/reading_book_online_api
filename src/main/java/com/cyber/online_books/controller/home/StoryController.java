43245f13f05bf54a658fee1536392b92c7072fdd
tin <tinsatthu1988@gmail.com>
2022-02-16 23:27:35 +0700

    get story home by id

--- src/main/java/com/cyber/online_books/controller/home/StoryController.java
+++ src/main/java/com/cyber/online_books/controller/home/StoryController.java
@@ -3,26 +3,22 @@ package com.cyber.online_books.controller.home;
 import com.cyber.online_books.entity.Chapter;
 import com.cyber.online_books.entity.User;
 import com.cyber.online_books.exception.ExceptionHandling;
+import com.cyber.online_books.exception.domain.HttpMyException;
 import com.cyber.online_books.exception.domain.UserNotFoundException;
-import com.cyber.online_books.exception.domain.UserNotLoginException;
 import com.cyber.online_books.response.*;
-import com.cyber.online_books.service.ChapterService;
-import com.cyber.online_books.service.StoryService;
-import com.cyber.online_books.service.UserService;
+import com.cyber.online_books.service.*;
 import com.cyber.online_books.utils.ConstantsListUtils;
+import com.cyber.online_books.utils.ConstantsStatusUtils;
 import com.cyber.online_books.utils.ConstantsUtils;
-import com.cyber.online_books.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
-import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/truyen-home")
@@ -31,31 +27,22 @@ public class StoryController extends ExceptionHandling {
     private final StoryService storyService;
     private final UserService userService;
     private final ChapterService chapterService;
+    private final UserRatingService userRatingService;
+    private final HistoryService historyService;
 
     @Autowired
-    public StoryController(StoryService storyService, UserService userService, ChapterService chapterService) {
+    public StoryController(StoryService storyService, UserService userService, ChapterService chapterService, UserRatingService userRatingService, HistoryService historyService) {
         this.storyService = storyService;
         this.userService = userService;
         this.chapterService = chapterService;
+        this.userRatingService = userRatingService;
+        this.historyService = historyService;
     }
 
     @GetMapping(value = "/{storyId}")
     public ResponseEntity< ? > defaultStoryController(@PathVariable("storyId") Long storyId, Principal principal) throws Exception {
 
-
-        // lấy truyện theo id va status
-        if (principal == null){
-            StorySummary storySummary = storyService.findStoryByStoryIdAndStatus(storyId,
-                    ConstantsListUtils.LIST_STORY_DISPLAY);
-
-            return new ResponseEntity<>(storySummary, HttpStatus.OK);
-        } else {
-            String currentUsername = principal.getName();
-            User user = userService.findUserAccount(currentUsername);
-            StorySummary storySummary = storyService.findStoryByStoryIdAndStatus(storyId,
-                    ConstantsListUtils.LIST_STORY_DISPLAY);
-            return new ResponseEntity<>(storySummary, HttpStatus.OK);
-        }
+        return new ResponseEntity<>(getStoryDetail(storyId, principal), HttpStatus.OK);
 
     }
 
@@ -85,6 +72,48 @@ public class StoryController extends ExceptionHandling {
                 .findStoryByUserId(userId, ConstantsListUtils.LIST_STORY_DISPLAY,
                         pagenumber, type, ConstantsUtils.PAGE_SIZE_DEFAULT);
         return new ResponseEntity<>(storyMembers, HttpStatus.OK);
+    }
+
+    private StoryDetailResponse getStoryDetail(Long storyId, Principal principal) throws Exception {
+        StoryDetailResponse storyDetailResponse = new StoryDetailResponse();
+
+        StorySummary storySummary = storyService.findStoryByStoryIdAndStatus(storyId,
+                ConstantsListUtils.LIST_STORY_DISPLAY);
+
+        boolean checkRating = false;
+        boolean checkConverter = false;
+        Chapter chapter = null;
+
+        if(principal != null){
+            String currentUsername = principal.getName();
+            User user = userService.findUserAccount(currentUsername);
+            if (user != null) {
+                chapter = historyService.findChapterReadByUser(user.getId(), storyId);
+                checkConverter = Objects.equals(user.getId(), storySummary.getUserId());
+                //Nếu người đọc là người đăng thì tính là đã đánh giá
+                if (storySummary.getUserId().equals(user.getId())) {
+                    // Người đọc là Converter
+                    checkRating = true;
+                } else {
+
+                    // Kiểm tra Người dùng đã đánh giá chưa
+                    if (userRatingService.existsRatingWithUser(storyId, user.getId())) {
+                        // Người dùng đã đánh giá
+                        checkRating = true;
+                    }
+                }
+            }
+        }
+
+        Long countRating = userRatingService.countRatingStory(storyId);
+
+        storyDetailResponse.setReadChapter(chapter);
+        storyDetailResponse.setStorySummary(storySummary);
+        storyDetailResponse.setCountRating(countRating);
+        storyDetailResponse.setRating(checkRating);
+        storyDetailResponse.setCheckConverter(checkConverter);
+
+        return storyDetailResponse;
     }
 
 }
